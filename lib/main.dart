593f98be388c885886c754357661ab8cff55fcf1
Pierre <bonnamy.77@gmail.com>
2021-05-18 12:16:48 +0200

    use phone controllers instead of previous integrated controllers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'navigate.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -22,33 +22,58 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Completer<WebViewController> _controller = Completer<WebViewController>();
+  Completer<WebViewController> _controllerCompleter = Completer<WebViewController>();
+
+  WebViewController _controller;
+
+  Future<void> _handleBack(context) async {
+    var status = await _controller.canGoBack();
+    if (status) {
+      _controller.goBack();
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Êtes-vous sûr de vouloir quitter ?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('NON'),
+              ),
+              TextButton(
+                onPressed: () {
+                  SystemNavigator.pop();
+                },
+                child: Text('OUI'),
+              ),
+            ],
+          ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text(widget.title),
-        actions: <Widget>[
-          NavigationControls(_controller.future),
-        ],
-      ),
-      body: WebView(
+      body:
+      WillPopScope(
+        onWillPop: () => _handleBack(context),
+        child : WebView(
         initialUrl: 'http://sosdrolesdedames.fr/',
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        },
+          onWebViewCreated: (WebViewController webViewController) {
+            _controllerCompleter.future.then((value) => _controller = value);
+            _controllerCompleter.complete(webViewController);
+          },
       ),
+    ),
     );
   }
 }
